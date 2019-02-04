6276b9f83f8feb824541806aed32ffb0697f37bf
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-04 04:28:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,21 @@
 def addition(num1, num2)
-end
+  puts 2 + 3
+end 
 
 def subtraction(num1, num2)
+  puts 5 - 4
 end
 
 def division(num1, num2)
+  puts 9 \ 3
 end
 
 def multiplication(num1, num2)
+ puts  5 * 3
 end
 
 def modulo(num1, num2)
+  puts Math.s
 end
 
 def square_root(num)
