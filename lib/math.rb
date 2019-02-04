999a7201defff3ac26fd25bcc4446db92c82e69c
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-04 04:31:24 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
-end
+  puts 2 + 3
+end 
 
 def subtraction(num1, num2)
+  puts 5 - 4
 end
 
 def division(num1, num2)
+  puts 9 \ 3
 end
 
 def multiplication(num1, num2)
+ puts  5 * 3
 end
 
 def modulo(num1, num2)
+  puts Math.sine(6)
 end
 
 def square_root(num)
-end
+  puts Math.sqrt(9)
+end 
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  #Hint:  2_ + (( 34_ * _2_ ) / _2_ )
 end
